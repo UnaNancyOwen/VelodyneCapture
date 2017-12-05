3a3e63aa80d835ed0819d14b592ca88864f8d0c6
Tsukasa Sugiura <t.sugiura0204@gmail.com>
2017-12-06 04:50:45 +0900

    Fix look up table for VLP-16
    Fix look up table for VLP-16.

--- sample/viewer/VelodyneCapture.h
+++ sample/viewer/VelodyneCapture.h
@@ -520,7 +520,7 @@ namespace velodyne
     {
         private:
             static const int MAX_NUM_LASERS = 16;
-            const std::vector<double> lut = { 15.0, 7.0, 14.0, 6.0, 13.0, 5.0, 12.0, 4.0, 11.0, 3.0, 10.0, 2.0, 9.0, 1.0, 8.0, 0.0 };
+            const std::vector<double> lut = { -15.0, 1.0, -13.0, -3, -11.0, 5.0, -9.0, 7.0, -7.0, 9.0, -5.0, 11.0, -3.0, 13.0, -1.0, 15.0 };
 
         public:
             VLP16Capture() : VelodyneCapture()
